857fb944c0c45291d2df8905d3d1b30f5e9d051e
bill7maxmaye <bilenmehalek7@gmail.com>
2024-03-27 13:44:59 +0300

    create book routes

--- backend/index.js
+++ backend/index.js
@@ -40,7 +40,18 @@ app.post("/books", async (req, res) => {
 app.get("/books", async (req, res) => {
   try {
     const books = await Book.find({});
-    res.status(200).send({ count: books.length, data: books });
+    res.status(200).json({ count: books.length, data: books });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: error.message });
+  }
+});
+
+app.get("/books/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const book = await Book.findById(id);
+    return res.status(200).send(book);
   } catch (error) {
     console.log(error);
     res.status(500).send({ message: error.message });
