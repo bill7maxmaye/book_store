e23fff25d2f6431b668b8aad5837a14bd982c25a
bill7maxmaye <bilenmehalek7@gmail.com>
2024-03-27 15:56:16 +0300

    created react app with vite and completed the api endpoints

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import { mongo_URL, PORT } from "./config.js";
 import { Book } from "./models/bookModel.js";
+import bookRoute from "./routes/bookRoutes.js";
 
 const app = express();
 app.use(express.json());
@@ -15,48 +16,7 @@ app.get("/", (req, res) => {
     );
 });
 
-app.post("/books", async (req, res) => {
-  try {
-    if (!req.body.title || !req.body.author || !req.body.publishedYear) {
-      return res
-        .status(400)
-        .send({ message: "send all required fields from the frontend" });
-    }
-
-    const newBook = {
-      title: req.body.title,
-      author: req.body.author,
-      publishedYear: req.body.publishedYear,
-    };
-
-    const book = await Book.create(newBook);
-    return res.status(201).send(book);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send({ message: error.message });
-  }
-});
-
-app.get("/books", async (req, res) => {
-  try {
-    const books = await Book.find({});
-    res.status(200).json({ count: books.length, data: books });
-  } catch (error) {
-    console.log(error);
-    res.status(500).send({ message: error.message });
-  }
-});
-
-app.get("/books/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const book = await Book.findById(id);
-    return res.status(200).send(book);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send({ message: error.message });
-  }
-});
+app.use("/books", bookRoute);
 
 mongoose
   .connect(mongo_URL)
