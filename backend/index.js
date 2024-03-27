0345bb453b6a40762604034884578bd6c2771475
bill7maxmaye <bilenmehalek7@gmail.com>
2024-03-27 13:36:11 +0300

    create book routes

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import mongoose from "mongoose";
 import { mongo_URL, PORT } from "./config.js";
+import { Book } from "./models/bookModel.js";
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
   console.log(req);
@@ -11,6 +13,38 @@ app.get("/", (req, res) => {
     .send(
       "the browser sent a get request to the default / home route and this is the response from the server"
     );
+});
+
+app.post("/books", async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.author || !req.body.publishedYear) {
+      return res
+        .status(400)
+        .send({ message: "send all required fields from the frontend" });
+    }
+
+    const newBook = {
+      title: req.body.title,
+      author: req.body.author,
+      publishedYear: req.body.publishedYear,
+    };
+
+    const book = await Book.create(newBook);
+    return res.status(201).send(book);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: error.message });
+  }
+});
+
+app.get("/books", async (req, res) => {
+  try {
+    const books = await Book.find({});
+    res.status(200).send({ count: books.length, data: books });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: error.message });
+  }
 });
 
 mongoose
@@ -22,5 +56,5 @@ mongoose
     });
   })
   .catch((error) => {
-    console.log(eror);
+    console.log(error);
   });
--- backend/models/bookModel.js
+++ backend/models/bookModel.js
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
 
-const bookSchema = new mongoose.Schema({
-  title: { type: String, required: true }, //String,
-  author: { type: String, required: true }, //String,
-  publishedYear: { type: Number, required: true }, //Number,
-  timeStamp: true,
-});
+const bookSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true }, //String,
+    author: { type: String, required: true }, //String,
+    publishedYear: { type: Number, required: true },
+  },
+  { timestamps: true } //Number,
+);
 
 export const Book = mongoose.model("Book", bookSchema);
