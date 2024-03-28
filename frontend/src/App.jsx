e1e9066587cde00e1fa45e79c8043b4494150cd7
bill7maxmaye <bilenmehalek7@gmail.com>
2024-03-28 10:28:22 +0300

    created the pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import CreateBook from "./pages/CreateBook";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import ShowBook from "./pages/ShowBook";
 import EditBook from "./pages/EditBook";
 import DeleteBook from "./pages/DeleteBook";
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Spinner from "../components/Spinner";
+
+const Home = () => {
+  return (
+    <div m-14>
+      <Spinner />
+    </div>
+  );
+};
+
+export default Home;
