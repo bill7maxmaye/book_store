b6f218320e7915bb5631f85ecf6cbb1e20e5540e
bill7maxmaye <bilenmehalek7@gmail.com>
2024-03-28 16:52:51 +0300

    completed create book page

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -19,12 +19,12 @@ const CreateBook = () => {
     };
     setLoading(true);
     axios
-      .post("http://localhos:3000/books", data)
+      .post("http://localhost:3000/books", data)
       .then(() => {
         setLoading(false);
         navigate("/");
       })
-      .catch(() => {
+      .catch((error) => {
         setLoading(false);
         alert("Something went wrong");
         console.log(error);
@@ -32,10 +32,10 @@ const CreateBook = () => {
   };
 
   return (
-    <div className=" p-4">
+    <div className="p-4">
       <BackButton />
-      <h1>Create AmDAker</h1>
-      {loadding ? <Spinner /> : ""}
+      <h1 className="text-3xl my-4">Create AmDAker</h1>
+      {loading ? <Spinner /> : ""}
 
       <div className=" flex flex-col border-2 border-sky-400 w-[600px] p-4 mx-auto">
         <div className="my-4">
@@ -47,6 +47,30 @@ const CreateBook = () => {
             className="border-2 border-gray-200 rounded-lg w-full p-2"
           />
         </div>
+
+        <div className="my-4">
+          <label className="text-xl mr-4 text-gray-500 ">Author</label>
+          <input
+            type="text"
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+            className="border-2 border-gray-200 rounded-lg w-full p-2"
+          />
+        </div>
+
+        <div className="my-4">
+          <label className="text-xl mr-4 text-gray-500 ">Publish Year</label>
+          <input
+            type="text"
+            value={publishedYear}
+            onChange={(e) => setPublishedYear(e.target.value)}
+            className="border-2 border-gray-200 rounded-lg w-full p-2"
+          />
+        </div>
+
+        <button className="p-2 bg-sky-300 m-8 " onClick={handleSaveBook}>
+          Save
+        </button>
       </div>
     </div>
   );
