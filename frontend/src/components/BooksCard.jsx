e481fccbf220abd03e2579996d3dc80783677ac7
bill7maxmaye <bilenmehalek7@gmail.com>
2024-03-30 10:17:21 +0300

    completed bookModal and alert

--- frontend/src/components/BooksCard.jsx
+++ frontend/src/components/BooksCard.jsx
@@ -1,23 +1,12 @@
 import React from "react";
+import BookSingleCard from "./BookSingleCard";
 
 const BooksCard = ({ books }) => {
-  // Check if books array exists and has at least one element
-  if (!books || books.length === 0) {
-    return <div className="text-center text-gray-600">No books available</div>;
-  }
-
   return (
-    <div className=" p-5  max-w-full mx-auto bg-white rounded-3xl shadow-md overflow-hidden">
-      <div className="text-center text-xl font-semibold bg-gray-200 py-4">
-        Book Titles
-      </div>
-      <ul className="divide-y divide-gray-200 max-h-80 overflow-y-auto">
-        {books.map((book) => (
-          <li key={book._id} className="px-4 py-2">
-            {book.title}
-          </li>
-        ))}
-      </ul>
+    <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      {books.map((item) => (
+        <BookSingleCard item={item} key={item._id} />
+      ))}
     </div>
   );
 };
--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -3,6 +3,7 @@ import BackButton from "../components/BackButton";
 import Spinner from "../components/Spinner";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { useSnackbar } from "notistack";
 
 const CreateBook = () => {
   const [title, setTitle] = useState("");
@@ -10,6 +11,7 @@ const CreateBook = () => {
   const [publishedYear, setPublishedYear] = useState("");
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
+  const { enqueueSnackbar } = useSnackbar();
 
   const handleSaveBook = () => {
     const data = {
@@ -22,6 +24,7 @@ const CreateBook = () => {
       .post("http://localhost:3000/books", data)
       .then(() => {
         setLoading(false);
+        enqueueSnackbar("Book created successfully", { variant: "success" });
         navigate("/");
       })
       .catch((error) => {
